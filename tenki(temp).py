0f4517cb88cb842d3c2a25b97a740d56a536f90b
snowywinter-code <athisanon142@gmail.com>
2026-05-29 14:25:26 +0900

    項目を選択

--- tenki(temp).py
+++ tenki(temp).py
@@ -2,17 +2,31 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
 df = pd.read_csv("tenki(temp).csv")
-date = df["date"].tolist()
-data = df["Avg"].tolist()
+while(True):
+  try:
+    item = str(input("Choose item (Avg / Max / Min)\n"))
+    date = df["date"].tolist()
+    data = df[item].tolist()
+  except KeyError:
+    print("please try again")
+  else:
+    break
+px = 1/plt.rcParams['figure.dpi']  # pixel in inches
+plt.figure(figsize=(1200*px, 800*px))
 plt.plot(date, data)
 plt.gca().xaxis.set_major_locator(MaxNLocator(nbins=12))
 plt.xlabel('date')
 plt.tick_params(axis='x', labelrotation=67)
 plt.ylabel('Average Tempurature [°C]')
-
+dict = {
+  "Avg": "Average",
+  "Max": "Maximum",
+  "Min": "Minimum"
+}
+title = "Daily "+dict[item]+" Tempurature"
 # giving a title to my graph
-plt.title('Daily Average Tempurature')
-
+plt.title(title)
+name = "tenki(temp)_"+item+".png"
 # function to show the plot
-plt.savefig("tenki(temp).png")
+plt.savefig(name)
 plt.show()
